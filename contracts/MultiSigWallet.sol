188b6813d70fd47762ad1f16f4a7ab62c72e75e6
andrew <fleming-andrew@protonmail.com>
2020-12-26 09:54:48 -0500

    remake contract to solc version 0.5.16

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.7.0;
+pragma solidity ^0.5.16;
 
 contract MultiSigWallet {
     
@@ -6,53 +6,24 @@ contract MultiSigWallet {
     mapping(address => bool) public isOwner;
     uint public numConfirmationsRequired;
     
-    struct Transaction {
-        address to;
-        uint value;
-        bytes data;
-        bool executed;
-        mapping(address => bool) isConfirmed;
-        uint numConfirmations;
-    }
-    
-    modifier onlyOwner() {
-        require(isOwner[msg.sender], "Not owner");
-        _;
-    }
-    
-    event Deposit(address indexed sender, uint amount, uint balance);
-    event SubmitTransaction(
-        address indexed owner,
-        uint indexed txIndex,
-        address indexed to,
-        uint value,
-        bytes data
-    );
-    
-    constructor(address[] memory _owners, uint _numConfirmationsRequired) {
-    require(_owners.length > 0, "owners required");
-    require(_numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,
-        "Invalid number of confirmations");
-        
-        for(uint i = 0; i <= _owners.length; i++){
+    constructor(address[] memory _owners, uint _numConfirmationsRequired) public {
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,
+            "invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
             address owner = _owners[i];
-            require(owner != address(0), "Invalid owner");
-            require(!isOwner[owner], "Owner not unique");
-            
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
             isOwner[owner] = true;
             owners.push(owner);
         }
-        
+
         numConfirmationsRequired = _numConfirmationsRequired;
-    }
-    
-    //for deposits
-    receive() external payable {
-        emit Deposit(msg.sender, msg.value, address(this).balance);
-    }
-    
-    function submitTransaction(address _to, uint _value, bytes memory _data) public onlyOwner {
-        
     }
     
     
